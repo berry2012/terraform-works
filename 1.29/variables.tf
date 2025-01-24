6574a74d5a9ccf034e97da58d0d11bf1260b4abd
EC2 Default User <ec2-user@ip-192-168-94-140.eu-west-1.compute.internal>
2025-01-24 14:31:53 +0000

    provision 1.29 cluster

---
+++ 1.29/variables.tf
@@ -0,0 +1,19 @@
+variable "cluster_name" {
+  description = "EKS Cluster Name"
+  type        = string
+  default     = "buildonaws"
+}
+
+
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  default     = "eu-west-2"
+}
+
+
+variable "enable_example" {
+  description = "Enable example to test this blueprint"
+  type        = bool
+  default     = true
+}
